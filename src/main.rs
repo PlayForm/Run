835641abcf9b712e92fb91ebe800274ea5a3ee63
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-08 05:02:59 +0200

    squash!

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+	#[arg(short, long)]
+	directories: Vec<String>,
+
+	/// Number of times to greet
+	#[arg(short, long, default_value_t = 1)]
+	count: u8,
+}
+
 fn main() {}
