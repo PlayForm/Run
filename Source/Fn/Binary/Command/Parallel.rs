52baec5b2ffb083306daf4fc8bcbc266ce3715d4
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-02 04:40:51 +0200

    squash!

--- Source/Fn/Binary/Command/Parallel.rs
+++ Source/Fn/Binary/Command/Parallel.rs
@@ -1,28 +1,17 @@
 pub fn Fn(Option { Entry, Separator, Pattern, Command, .. }: Option) {
 	let mut Queue = Vec::new();
 
-	for Entry in Entry
-		.into_iter()
-		.map(|Entry| match Entry.last() {
-			Some(Last) => {
-				if *Last == Pattern {
-					Some(Entry[0..Entry.len() - 1].join(&Separator.to_string()))
-				} else {
-					None
-				}
-			}
-			None => None,
-		})
-		.filter_map(|Entry| Entry)
-	{
-		let Output;
-
-		if cfg!(target_os = "windows") {
-			Output =
-				CommandTokio::new("cmd").args(["/C", Command.as_str()]).current_dir(Entry).output();
+	for Entry in Entry.into_iter().filter_map(|Entry| {
+		Entry
+			.last()
+			.filter(|Last| *Last == &Pattern)
+			.map(|_| Entry[0..Entry.len() - 1].join(&Separator.to_string()))
+	}) {
+		let Output = if cfg!(target_os = "windows") {
+			CommandTokio::new("cmd").args(["/C", Command.as_str()]).current_dir(Entry).output()
 		} else {
-			Output = CommandTokio::new("sh").arg("-c").current_dir(Entry).arg(&Command).output();
-		}
+			CommandTokio::new("sh").arg("-c").current_dir(Entry).arg(&Command).output()
+		};
 
 		Queue.push(async move {
 			println!(
