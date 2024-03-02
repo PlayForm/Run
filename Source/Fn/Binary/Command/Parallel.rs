1d1e1053feaaa226726aea72ef306fcfdfb5ffe0
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-02 02:40:05 +0200

    squash!

--- Source/Fn/Binary/Command/Parallel.rs
+++ Source/Fn/Binary/Command/Parallel.rs
@@ -1,5 +1,11 @@
 pub fn Fn(Option { Entry, Separator, Pattern, Command, .. }: Option) {
 	let mut Queue = Vec::new();
+	let Deque = std::sync::Arc::new(std::sync::Mutex::new(deque::Worker::new_fifo()));
+	let mut Stealer = Vec::new();
+
+	for _ in 0..6 {
+		Stealer.push(Deque.lock().unwrap().stealer());
+	}
 
 	for Entry in Entry
 		.into_iter()
@@ -16,6 +22,7 @@ pub fn Fn(Option { Entry, Separator, Pattern, Command, .. }: Option) {
 		.filter_map(|Entry| Entry)
 	{
 		let Output;
+		let Deque = std::sync::Arc::clone(&Deque);
 
 		if cfg!(target_os = "windows") {
 			Output =
@@ -25,10 +32,7 @@ pub fn Fn(Option { Entry, Separator, Pattern, Command, .. }: Option) {
 		}
 
 		Queue.push(async move {
-			println!(
-				"{}",
-				String::from_utf8_lossy(&Output.await.expect("Failed to execute process.").stdout)
-			);
+			Deque.lock().unwrap().push(Output.await.expect("Failed to execute process.").stdout);
 		});
 	}
 
@@ -37,8 +41,21 @@ pub fn Fn(Option { Entry, Separator, Pattern, Command, .. }: Option) {
 			Queue.await;
 		}
 	});
+
+	for Stealer in Stealer {
+		loop {
+			match Stealer.steal() {
+				deque::Steal::Success(Success) => {
+					println!("{}", String::from_utf8_lossy(&Success));
+				}
+				deque::Steal::Empty => break,
+				deque::Steal::Retry => continue,
+			}
+		}
+	}
 }
 
 use crate::Struct::Binary::Command::Entry::Struct as Option;
 
+use crossbeam::deque;
 use tokio::process::Command as CommandTokio;
