ca2e67cfab37af2d4b1becdd71e4a51d3f14d01d
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-03 23:49:49 +0200

    0.1.6

--- Source/Fn/Binary/Command/Parallel.rs
+++ Source/Fn/Binary/Command/Parallel.rs
@@ -1,31 +1,36 @@
 pub fn Fn(Option { Entry, Separator, Pattern, Command, .. }: Option) {
 	let mut Queue = Vec::new();
 
-	for Entry in Entry.into_iter().filter_map(|Entry| {
-		Entry
-			.last()
-			.filter(|Last| *Last == &Pattern)
-			.map(|_| Entry[0..Entry.len() - 1].join(&Separator.to_string()))
-	}) {
-		let Output = if cfg!(target_os = "windows") {
-			CommandTokio::new("cmd").args(["/C", Command.as_str()]).current_dir(Entry).output()
-		} else {
-			CommandTokio::new("sh").arg("-c").current_dir(Entry).arg(&Command).output()
-		};
+	Entry
+		.into_iter()
+		.filter_map(|Entry| {
+			Entry
+				.last()
+				.filter(|Last| *Last == &Pattern)
+				.map(|_| Entry[0..Entry.len() - 1].join(&Separator.to_string()))
+		})
+		.for_each(|Entry| {
+			let Output = if cfg!(target_os = "windows") {
+				CommandTokio::new("cmd").args(["/C", Command.as_str()]).current_dir(Entry).output()
+			} else {
+				CommandTokio::new("sh").arg("-c").current_dir(Entry).arg(&Command).output()
+			};
 
-		Queue.push(async move {
-			println!(
-				"{}",
-				String::from_utf8_lossy(&Output.await.expect("Failed to execute process.").stdout)
-			);
+			Queue.push(async move {
+				println!(
+					"{}",
+					String::from_utf8_lossy(
+						&Output.await.expect("Failed to execute process.").stdout
+					)
+				);
+			});
 		});
-	}
 
 	tokio::runtime::Runtime::new().expect("Cannot Runtime.").block_on(async {
 		for Queue in Queue {
 			Queue.await;
 		}
-	});
+	})
 }
 
 use crate::Struct::Binary::Command::Entry::Struct as Option;
--- Source/Fn/Binary/Command/Sequential.rs
+++ Source/Fn/Binary/Command/Sequential.rs
@@ -1,44 +1,46 @@
 pub fn Fn(Option { Command, Entry, Pattern, Separator, .. }: Option) {
-	for Entry in Entry {
-		if let Some(Last) = Entry.last() {
-			if *Last == Pattern {
-				let Directory = &Entry[0..Entry.len() - 1].join(&Separator.to_string());
-
-				let mut Out = match cfg!(target_os = "windows") {
-					true => Command::new("cmd")
-						.args(["/C", &Command])
-						.current_dir(Directory)
-						.stdout(Stdio::piped())
-						.spawn()
-						.expect("Failed to execute process."),
-					false => Command::new("sh")
-						.arg("-c")
-						.current_dir(Directory)
-						.arg(Command.clone())
-						.stdout(Stdio::piped())
-						.spawn()
-						.expect("Failed to execute process."),
-				}
-				.stdout
-				.expect("Failed to get stdout handle");
-
-				let mut Output = String::new();
-
-				loop {
-					let mut Buffer = [0; 512];
-					let Byte = Out.read(&mut Buffer).expect("Failed to read from pipe");
-
-					if Byte == 0 {
-						break;
-					}
-
-					Output.push_str(&String::from_utf8_lossy(&Buffer[..Byte]));
-				}
-
-				println!("{}", Output);
+	Entry
+		.into_iter()
+		.filter_map(|Entry| {
+			Entry
+				.last()
+				.filter(|Last| *Last == &Pattern)
+				.map(|_| Entry[0..Entry.len() - 1].join(&Separator.to_string()))
+		})
+		.for_each(|Entry| {
+			let mut Out = match cfg!(target_os = "windows") {
+				true => Command::new("cmd")
+					.args(["/C", &Command])
+					.current_dir(Entry)
+					.stdout(Stdio::piped())
+					.spawn()
+					.expect("Failed to execute process."),
+				false => Command::new("sh")
+					.arg("-c")
+					.current_dir(Entry)
+					.arg(Command.clone())
+					.stdout(Stdio::piped())
+					.spawn()
+					.expect("Failed to execute process."),
 			}
-		}
-	}
+			.stdout
+			.expect("Failed to get stdout handle");
+
+			let mut Output = String::new();
+
+			loop {
+				let mut Buffer = [0; 512];
+				let Byte = Out.read(&mut Buffer).expect("Failed to read from pipe");
+
+				if Byte == 0 {
+					break;
+				}
+
+				Output.push_str(&String::from_utf8_lossy(&Buffer[..Byte]));
+			}
+
+			println!("{}", Output);
+		})
 }
 
 use crate::Struct::Binary::Command::Entry::Struct as Option;
