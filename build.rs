9e129b76e813be1943ba3687fb7291f801d933c3
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-06 12:44:05 +0300

    squash!

--- build.rs
+++ build.rs
@@ -1,20 +1,26 @@
 #![allow(non_snake_case)]
 
+use serde::Deserialize;
 use std::fs;
+
+#[derive(Deserialize)]
+struct Toml {
+	package: Package,
+}
+
+#[derive(Deserialize)]
+struct Package {
+	version: String,
+}
 
 fn main() {
 	println!("cargo:rerun-if-changed=Cargo.toml");
+
 	println!(
 		"cargo:rustc-env=CARGO_PKG_VERSION={}",
-		fs::read_to_string("Cargo.toml")
-			.expect("Cannot Cargo.toml.")
-			.lines()
-			.find(|Line| Line.starts_with("version"))
-			.expect("Cannot Version.")
-			.split('=')
-			.nth(1)
-			.expect("Cannot nth.")
-			.trim()
-			.trim_matches('"')
+		(toml::from_str::<Toml>(&fs::read_to_string("Cargo.toml").expect("Cannot Cargo.toml."))
+			.expect("Cannot toml."))
+		.package
+		.version
 	);
 }
